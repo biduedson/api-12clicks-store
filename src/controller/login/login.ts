fc314b8d37f2453215e5819daefb28ddfa0743a8
biduedson <112134265+biduedson@users.noreply.github.com>
2023-12-05 17:01:05 -0300

    feat:create login controller class

---
+++ src/controller/login/login.ts
@@ -0,0 +1,45 @@
+import { IloggedInUser } from "../../interfaces/generate-token/generate-token";
+import { HttpRequest, HttpResponse } from "../../interfaces/http/http";
+import {
+  IloginUserController,
+  IloginUserParams,
+  iLoginUserRepository,
+} from "../../interfaces/login/login";
+
+export class LoginUserController implements IloginUserController {
+  constructor(private readonly loginUserRepository: iLoginUserRepository) {}
+  async handle(
+    httpRequest: HttpRequest<IloginUserParams>
+  ): Promise<HttpResponse<IloggedInUser>> {
+    try {
+      const { email, password } = httpRequest.body!;
+
+      const checkEmailAndPassword =
+        await this.loginUserRepository.checkEmailAndPassword({
+          email: email,
+          password: password,
+        });
+
+      if (!checkEmailAndPassword) {
+        return {
+          statusCode: 400,
+          body: "Email ou senha incorretos.",
+        };
+      }
+
+      const userLogged = await this.loginUserRepository.createLogin(
+        httpRequest.body!
+      );
+
+      return {
+        statusCode: 200,
+        body: userLogged,
+      };
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: `Erro interno do servidor. ${error}`,
+      };
+    }
+  }
+}
