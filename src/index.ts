ef084e4ca58d7089363600e2eb61d0c3a751ede1
biduedson <112134265+biduedson@users.noreply.github.com>
2023-12-05 13:21:35 -0300

    feat: configuring server

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import "dotenv/config";
 import express from "express";
+import routes from "./routes";
 
 const app = express();
 app.use(express.json());
+app.use(routes);
 
 app.listen("4200", () => {
   console.log("Servidor rodando na porta 4200");
