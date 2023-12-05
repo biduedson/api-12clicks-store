543574354b1bf2ec8a01aafa14c064e00e49d33b
biduedson <112134265+biduedson@users.noreply.github.com>
2023-12-05 14:02:18 -0300

    feat: create route for all administrators

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,21 @@
 import { Request, Response, Router } from "express";
 import { CreateAdmUserRepository } from "./repositories/create-adm-user/create-adm-user";
 import { CreateUserAdmController } from "./controller/create-adm-user/create-adm-user";
+import { GetAdmUsersController } from "./controller/get-adm-user/get-adm-user";
+import { GetAdmUsersRepository } from "./repositories/get-adm-user/get-adm-user";
 
 const routes = Router();
 
 export default routes;
+
+routes.get("/administrators", async (req: Request, res: Response) => {
+  const getAdmUserReporitory = new GetAdmUsersRepository();
+  const getAdmUsersController = new GetAdmUsersController(getAdmUserReporitory);
+
+  const { statusCode, body } = await getAdmUsersController.handle();
+
+  res.status(statusCode).send(body);
+});
 
 routes.post("/administrators", async (req: Request, res: Response) => {
   const createAdmUserRepository = new CreateAdmUserRepository();
