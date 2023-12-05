d9f73a68f51ec858acc282c5a2b55e6e5e49b3c6
biduedson <112134265+biduedson@users.noreply.github.com>
2023-12-05 14:01:26 -0300

    feat: add class responsible for retorning all administrators

---
+++ src/repositories/get-adm-user/get-adm-user.ts
@@ -0,0 +1,20 @@
+import { IgetUserRepository } from "../../interfaces/get-adm-user/get-adm-user";
+import prisma from "../../services/prisma-client/prisma-client";
+
+export class GetAdmUsersRepository implements IgetUserRepository {
+  async getUsers(): Promise<
+    Omit<
+      { id: number; user: string; email: string; password: string },
+      "password"
+    >[]
+  > {
+    const admUsers = await prisma.administrators.findMany({
+      select: {
+        id: true,
+        user: true,
+        email: true,
+      },
+    });
+    return admUsers;
+  }
+}
