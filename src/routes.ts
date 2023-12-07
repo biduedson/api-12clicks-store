7a5999e67ad3c6e3f5a5a3f9282fbe1f22b1ff7a
biduedson <112134265+biduedson@users.noreply.github.com>
2023-12-07 17:50:26 -0300

    feat: create seach product on id route

--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,8 @@ import { getAdmUsersController } from "./controller/get-adm-user/get-adm-user";
 import { loginUserController } from "./controller/login/login";
 import { getProductcontroller } from "./controller/get-products/get-products";
 import { jwtValidator } from "./middleware/jwt-validation/jwt-validation";
+import { getOrderController } from "./controller/get-order/get-order";
+import { searchProductController } from "./controller/search-product/search-product";
 
 const routes = Router();
 
@@ -34,5 +36,17 @@ routes.post("/administrators", async (req: Request, res: Response) => {
 
 routes.get("/product", async (req: Request, res: Response) => {
   const { statusCode, body } = await getProductcontroller.handle();
+  res.status(statusCode).send(body);
+});
+
+routes.get("/product/:id", async (req: Request, res: Response) => {
+  const { statusCode, body } = await searchProductController.handle({
+    params: req.params,
+  });
+  res.status(statusCode).send(body);
+});
+
+routes.get("/order", async (req: Request, res: Response) => {
+  const { statusCode, body } = await getOrderController.handle();
   res.status(statusCode).send(body);
 });
